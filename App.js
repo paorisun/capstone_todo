5ac062356c1684ef55cf094596a638b01e6e20d7
unknown <sonada903@gmail.com>
2023-03-15 19:20:37 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>정처기 공부해라 다형아 뭐하냐</Text>
+      
+      <StatusBar style="auto" />
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
