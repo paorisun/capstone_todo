c89f7ff0df17db6fd5579e63163e72e83d80f0fa
Motoong <sonada903@gmail.com>
2023-03-16 15:57:09 +0900

    frame set

--- App.js
+++ App.js
@@ -1,22 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>정처기 공부해라 다형아 뭐하냐</Text>
-      
-      <StatusBar style="auto" />
+      <View style={styles.topFrame}>
+        <Text style={styles.topFrameToday}>
+          2023년 3월
+        </Text>
+      </View>
+      <View style={styles.middleFrame}>
+        <Text style={styles.contents}>
+          contents
+        </Text>    
+      </View>
+      <View style={styles.bottomFrame}>
+        <Text style={styles.menuBar}>
+          메뉴바
+        </Text>    
+      </View>
     </View>
-    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  topFrame: {
+    width: '100%',
+    height: '15%',
     justifyContent: 'center',
+    paddingLeft: 20,
+  },
+  topFrameToday: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'left',
+  },
+  middleFrame: {
+    width: '100%',
+    height: '70%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  contents: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  bottomFrame: {
+    width: '100%',
+    height: '15%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'gray',
+  },
+  menuBar: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
+
+
